5e8af7e2b68ea7d978d254ad0cecd2cf9ed1a368
tp <60010482@kmitl.ac.th>
2023-11-15 01:14:17 +0700

    organization

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,11 +1,17 @@
-import { OrganizationList } from "@clerk/nextjs";
+import { OrganizationList, UserButton } from "@clerk/nextjs";
 
 export default function CreateOrganizationPage() {
   return (
-    <OrganizationList
-      hidePersonal
-      afterSelectOrganizationUrl='/organization/:id'
-      afterCreateOrganizationUrl='/organization/:id'
-    />
+    <div className='relative'>
+      <div className='absolute z-10 right-4 top-4 xs:right-10'>
+        <UserButton afterSignOutUrl='/' />
+      </div>
+
+      <OrganizationList
+        hidePersonal
+        afterSelectOrganizationUrl='/organization/:id'
+        afterCreateOrganizationUrl='/organization/:id'
+      />
+    </div>
   );
 }

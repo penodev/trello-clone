6c54918f296f46c4429a616db8f2ba61a2867aef
tp <60010482@kmitl.ac.th>
2023-11-15 12:51:25 +0700

    add sidebar skeleton

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -100,5 +101,16 @@ export const NavItem = ({
         ))}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className='flex items-center gap-x-2'>
+      <div className='w-10 h-10 relative shrink-0'>
+        <Skeleton className='h-full w-full absolute' />
+      </div>
+      <Skeleton className='w-full h-10' />
+    </div>
   );
 };

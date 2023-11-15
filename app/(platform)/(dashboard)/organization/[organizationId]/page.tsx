8030b5130df0c424ad85aca31df50e77570dbd5d
tp <60010482@kmitl.ac.th>
2023-11-15 16:40:26 +0700

    server prisma db

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,21 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+
 const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id='title'
+          name='title'
+          required
+          placeholder='Enter a boarder title'
+          className='border-black border p-1'
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;

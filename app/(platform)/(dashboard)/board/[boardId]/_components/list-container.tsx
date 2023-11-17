601fb477f54d6f7df2bf017801bbdc5b22af17c6
tp <60010482@kmitl.ac.th>
2023-11-17 14:03:51 +0700

    List Component

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { List } from "@prisma/client";
+import { ListForm } from "./list-form";
+
+interface ListContainerProps {
+  data: List[];
+  boardId: string;
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  return (
+    <ol>
+      <ListForm />
+      <div className='flex-shrink-0 w-1'></div>
+    </ol>
+  );
+};
